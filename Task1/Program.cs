c6b212c8ef257b73e994cc0e076712da2b26b2bd
Basalyko Andrey <basalykoa.s@gmail.com>
2022-07-31 19:03:14 +0300

    Сделал первую задачу.

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число...");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write(N);
+Console.Write("--> ");
+int numbers(int m)
+{
+    if (m == 1) return N;
+    else return numbers(m - 1) - 1;
+}
+for(int i = 1; i < N + 1; ++i)
+{
+Console.Write(numbers(i));
+Console.Write(";");
+}
